9265531fe75ca83a3fe6b308239482325ea63da5
Jesse Lucas <jesse@forestgiant.com>
2015-08-12 04:10:01 -0400

    Simple application to look for the nearest /bin directory (above only) and call an command inside of if it.

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	// "io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	var appname string
+
+	// Check arg for appname to load
+	if len(os.Args) > 1 {
+		appname = os.Args[1]
+	} else {
+		log.Fatal("Must pass appname as argument")
+	}
+
+	fmt.Println(appname)
+
+	checkForBin(appname)
+
+}
+
+func checkForBin(appname string) {
+	dirname := "." + string(filepath.Separator)
+
+	d, err := os.Open(dirname)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer d.Close()
+
+	files, err := d.Readdir(-1)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.Mode().IsDir() {
+			if file.Name() == "bin" {
+				dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Println("Found Bin in: ", dir)
+
+				// set env for this bin path
+				v := fmt.Sprintf("$PATH:%v/bin", dir)
+				fmt.Println(v)
+
+				// TODO: Investigate solution to set environment variables to parent shell?
+				// path := "PATH=" + fmt.Sprintf("$PATH:%v/bin", dir)
+				// filename := "temp.sh"
+				// err = ioutil.WriteFile(filename, []byte(path), 0777)
+				// if err != nil {
+				// 	log.Fatal(err)
+				// }
+
+				// cmd := exec.Command("source", filename)
+				// cmd.Stdin = os.Stdin
+				// cmd.Stdout = os.Stdout
+				// cmd.Stderr = os.Stderr
+				// err = cmd.Run()
+				// if _, ok := err.(*exec.ExitError); ok {
+				// 	log.Fatal(err)
+				// }
+
+				os.Setenv("PATH", v)
+
+				// Call command
+				cmd := exec.Command(appname)
+				cmd.Stdin = os.Stdin
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				err = cmd.Run()
+				if _, ok := err.(*exec.ExitError); ok {
+					log.Fatal(err)
+				}
+
+				os.Exit(0)
+			}
+		}
+	}
+
+	// if it doesn't move up a directory and test
+	os.Chdir(".." + string(filepath.Separator))
+
+	checkForBin(appname)
+}
